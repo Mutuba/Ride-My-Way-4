38896034b41a84bc81a5a0a8994669524396c4f8
Mutuba <dmutuba@gmail.com>
2018-08-07 22:26:21 +0300

    [Feature-#159606241] Creates get ride endpoint

---
+++ get_ride.js
@@ -0,0 +1,57 @@
+document.getElementById('get_ride_form').addEventListener('submit', getRide);
+
+function getRide(e) {
+    e.preventDefault();
+
+    ride_id = document.getElementById('getRideInput').value;
+
+    fetch(`https://rmutubarides.herokuapp.com/api/v2/rides/${ride_id}`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json', "access-token": window.localStorage.getItem("token")}
+    })
+
+        .then(res => res.json())
+        //console.log(data);
+        .then(data => {
+            console.log(data);
+
+            let outpout = `<section id="rides-given-dashboard">
+                            <div class="container" >
+                                  <h3 id="message"></h3>
+                                    <table>
+                                      <thead>
+                                        <tr>
+
+                                          <th scope="col">Ride ID</th>
+                                          <th scope="col">Ride Date</th>
+                                          <th scope="col">Category</th>
+                                          <th scope="col">Pick Up</th>
+                                          <th scope="col">Drop Off</th>
+                                          <th scope="col">Date</th>
+                                          <th scope="col">Ride Status</th>
+                            
+                                        </tr>
+                                      </thead>
+                                      <tbody id="rides">
+                                        <tr>
+                                          <td>${data[0]["ride_id"]}</td>
+                                          <td>${data[0]["ride_date"]}</td>
+                                          <td>${data[0]["category"]}</td>
+                                          <td>${data[0]["pick_up"]}</td>
+                                          <td>${data[0]["drop_off"]}</td>
+                                          <td>${data[0]["date_time"]}</td>
+                                          <td>${data[0]["ride_status"]}</td>
+                                        
+                                          
+                                        </tr>
+                                      </tbody>
+
+                                    </table>
+                                  </div>
+
+                                </div>
+                            </section>`;
+            //console.log(data[0]["ride_id"]);
+            document.getElementById('rideDetails').innerHTML = outpout
+        })
+}
